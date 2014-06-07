815333dddce131ce81527a6fbe6600e457370978
Justin Mullis <justin.mullis@gmail.com>
2014-06-07 12:16:52 -0400

    changed max lenth to keith wood's plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+//= require ../../../vendor/assets/javascripts/jquery.min
+//= require ../../../vendor/assets/javascripts/jquery.plugin
+//= require ../../../vendor/assets/javascripts/jquery.maxlength
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
